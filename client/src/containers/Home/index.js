2ecaaef7443186f5718eb8d29bb9e6f0ec815a2c
Mike Meyerson <michael.b.meyerson@aexp.com>
2017-12-13 14:35:43 -0500

    landing page edits

--- client/src/containers/Home/index.js
+++ client/src/containers/Home/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { PageHeader, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { invokeApig } from '../../libs/aws-lib';
 
@@ -62,6 +63,14 @@ class Home extends Component {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     )
   }
